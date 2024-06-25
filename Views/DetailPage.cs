ae497519f5a4aab1960ffa109426ecc6ea6ca87a
Richard Woollcott <richard.woollcott@appmilla.co.uk>
2024-06-25 13:02:20 +0100

    Views defined in functions

--- Views/DetailPage.cs
+++ Views/DetailPage.cs
@@ -10,14 +10,32 @@ public class DetailPage : ReactiveContentPage<DetailPageViewModel>
     public DetailPage(DetailPageViewModel detailPageViewModel)
     {
         // Set the binding context
-        BindingContext = detailPageViewModel;
-        ViewModel = detailPageViewModel;
+        BindingContext = ViewModel = detailPageViewModel;
 
         // Set up the page title binding
         this.Bind(ViewModel, vm => vm.PageTitle, v => v.Title);
+
+        // Create the main layout
+        var scrollView = CreateScrollView();
         
-        var scrollView = new ScrollView();
-        
+        // Set the content of the page
+        Content = scrollView;
+
+        this.WhenActivated(disposables =>
+        {
+            // Any disposables here
+        });
+    }
+
+    private static ScrollView CreateScrollView()
+    {
+        var grid = CreateGrid();
+        var scrollView = new ScrollView { Content = grid };
+        return scrollView;
+    }
+
+    private static Grid CreateGrid()
+    {
         var grid = new Grid
         {
             Padding = new Thickness(0)
@@ -26,16 +44,37 @@ public class DetailPage : ReactiveContentPage<DetailPageViewModel>
         grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(70) });
         grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(3000) });
         grid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Star });
-        
+
+        var image = CreateImage();
+        grid.Children.Add(image);
+        Grid.SetRow(image, 0);
+        Grid.SetColumn(image, 0);
+
+        var nameLabel = CreateNameLabel();
+        grid.Children.Add(nameLabel);
+        Grid.SetRow(nameLabel, 1);
+        Grid.SetColumn(nameLabel, 0);
+
+        var webView = CreateWebView();
+        grid.Children.Add(webView);
+        Grid.SetRow(webView, 2);
+        Grid.SetColumn(webView, 0);
+
+        return grid;
+    }
+
+    private static Image CreateImage()
+    {
         var image = new Image
         {
             HorizontalOptions = LayoutOptions.Center
         };
         image.SetBinding(Image.SourceProperty, "CrewMember.Image");
-        grid.Children.Add(image);
-        Grid.SetRow(image, 0);
-        Grid.SetColumn(image, 0);
-        
+        return image;
+    }
+
+    private static Label CreateNameLabel()
+    {
         var nameLabel = new Label
         {
             Padding = new Thickness(10),
@@ -43,28 +82,16 @@ public class DetailPage : ReactiveContentPage<DetailPageViewModel>
             HorizontalOptions = LayoutOptions.Center
         };
         nameLabel.SetBinding(Label.TextProperty, "CrewMember.Name");
-        grid.Children.Add(nameLabel);
-        Grid.SetRow(nameLabel, 1);
-        Grid.SetColumn(nameLabel, 0);
-        
+        return nameLabel;
+    }
+
+    private static WebView CreateWebView()
+    {
         var webView = new WebView
         {
             VerticalOptions = LayoutOptions.Fill
         };
         webView.SetBinding(WebView.SourceProperty, "CrewMember.Wikipedia");
-        grid.Children.Add(webView);
-        Grid.SetRow(webView, 2);
-        Grid.SetColumn(webView, 0);
-
-        // Add the Grid to the ScrollView
-        scrollView.Content = grid;
-
-        // Set the content of the page
-        Content = scrollView;
-
-        this.WhenActivated(disposables =>
-        {
-            // Any disposables here
-        });
+        return webView;
     }
 }
--- Views/MainPage.cs
+++ Views/MainPage.cs
@@ -9,36 +9,57 @@ public class MainPage : ReactiveContentPage<MainPageViewModel>
 {
     public MainPage(MainPageViewModel mainPageViewModel)
     {
-        // Set the binding context
-        BindingContext = mainPageViewModel;
-        ViewModel = mainPageViewModel;
-
-        // Set up the page title binding
+        BindingContext = ViewModel = mainPageViewModel;
         this.Bind(ViewModel, vm => vm.PageTitle, v => v.Title);
-        
+
+        Content = CreateRefreshView();
+
+        this.WhenActivated(disposables =>
+        {
+            // Any disposables here
+        });
+    }
+
+    private RefreshView CreateRefreshView()
+    {
         var refreshView = new RefreshView
         {
-            Command = ViewModel.LoadCommand,
+            Command = ViewModel?.LoadCommand,
             CommandParameter = true
         };
-        refreshView.SetBinding(RefreshView.IsRefreshingProperty, "IsRefreshing");
-        
-        var scrollView = new ScrollView();
+        refreshView.SetBinding(RefreshView.IsRefreshingProperty, nameof(ViewModel.IsRefreshing));
+        refreshView.Content = CreateScrollView();
+
+        return refreshView;
+    }
+
+    private ScrollView CreateScrollView()
+    {
+        var scrollView = new ScrollView
+        {
+            Content = CreateCollectionView()
+        };
+        return scrollView;
+    }
+
+    private CollectionView CreateCollectionView()
+    {
         var collectionView = new CollectionView
         {
             SelectionMode = SelectionMode.None,
             EmptyView = "Please pull to refresh the view",
-            Margin = new Thickness(10)
+            Margin = new Thickness(10),
+            ItemsLayout = new LinearItemsLayout(ItemsLayoutOrientation.Vertical) { ItemSpacing = 10 },
+            ItemTemplate = CreateItemTemplate()
         };
-        collectionView.SetBinding(ItemsView.ItemsSourceProperty, "Crew");
-        
-        var itemsLayout = new LinearItemsLayout(ItemsLayoutOrientation.Vertical)
-        {
-            ItemSpacing = 10
-        };
-        collectionView.ItemsLayout = itemsLayout;
-        
-        collectionView.ItemTemplate = new DataTemplate(() =>
+        collectionView.SetBinding(ItemsView.ItemsSourceProperty, nameof(ViewModel.Crew));
+
+        return collectionView;
+    }
+
+    private DataTemplate CreateItemTemplate()
+    {
+        return new DataTemplate(() =>
         {
             var frame = new Frame
             {
@@ -49,79 +70,89 @@ public class MainPage : ReactiveContentPage<MainPageViewModel>
                 HorizontalOptions = LayoutOptions.Fill
             };
 
-            var grid = new Grid
-            {
-                Padding = new Thickness(0)
-            };
-            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(80) });
-            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(50) });
-            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(160) });
-            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
-
-            var tapGestureRecognizer = new TapGestureRecognizer();
-            tapGestureRecognizer.SetBinding(TapGestureRecognizer.CommandProperty, new Binding("BindingContext.NavigateToDetailCommand", source: this));
-            tapGestureRecognizer.SetBinding(TapGestureRecognizer.CommandParameterProperty, ".");
-            grid.GestureRecognizers.Add(tapGestureRecognizer);
-
-            var image = new Image
-            {
-                Aspect = Aspect.AspectFill,
-                HeightRequest = 150,
-                WidthRequest = 150,
-                Margin = new Thickness(0)
-            };
-            image.SetBinding(Image.SourceProperty, "Image");
-
-            var nameLabel = new Label
-            {
-                FontAttributes = FontAttributes.Bold,
-                FontSize = 18,
-                VerticalOptions = LayoutOptions.Start,
-                Margin = new Thickness(10, 10, 10, 0)
-            };
-            nameLabel.SetBinding(Label.TextProperty, "Name");
-
-            var agencyLabel = new Label
-            {
-                FontSize = 16,
-                Margin = new Thickness(10, 0, 10, 10)
-            };
-            agencyLabel.SetBinding(Label.TextProperty, "Agency");
-
-            grid.Children.Add(image);
-            grid.Children.Add(nameLabel);
-            grid.Children.Add(agencyLabel);
-
-            Grid.SetRow(nameLabel, 0);
-            Grid.SetColumn(nameLabel, 1);
-            Grid.SetRow(agencyLabel, 1);
-            Grid.SetColumn(agencyLabel, 1);
-
-            Grid.SetRow(image, 0);
-            Grid.SetColumn(image, 0);
-            Grid.SetRowSpan(image, 2);
-            
+            var grid = CreateGrid();
             frame.Content = grid;
 
             return frame;
         });
-
-        // Add the CollectionView to the ScrollView and then to the RefreshView
-        scrollView.Content = collectionView;
-        refreshView.Content = scrollView;
-
-        // Set the content of the page
-        Content = refreshView;
-        
-        this.WhenActivated(disposables =>
-        {
-        });
     }
-    
+
+    private Grid CreateGrid()
+    {
+        var grid = new Grid
+        {
+            Padding = new Thickness(0)
+        };
+        grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(80) });
+        grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(50) });
+        grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(160) });
+        grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+
+        var tapGestureRecognizer = new TapGestureRecognizer();
+        tapGestureRecognizer.SetBinding(TapGestureRecognizer.CommandProperty, new Binding("BindingContext.NavigateToDetailCommand", source: this));
+        tapGestureRecognizer.SetBinding(TapGestureRecognizer.CommandParameterProperty, ".");
+        grid.GestureRecognizers.Add(tapGestureRecognizer);
+
+        var image = CreateImage();
+        var nameLabel = CreateNameLabel();
+        var agencyLabel = CreateAgencyLabel();
+
+        grid.Children.Add(image);
+        grid.Children.Add(nameLabel);
+        grid.Children.Add(agencyLabel);
+
+        Grid.SetRow(nameLabel, 0);
+        Grid.SetColumn(nameLabel, 1);
+        Grid.SetRow(agencyLabel, 1);
+        Grid.SetColumn(agencyLabel, 1);
+
+        Grid.SetRow(image, 0);
+        Grid.SetColumn(image, 0);
+        Grid.SetRowSpan(image, 2);
+
+        return grid;
+    }
+
+    private static Image CreateImage()
+    {
+        var image = new Image
+        {
+            Aspect = Aspect.AspectFill,
+            HeightRequest = 150,
+            WidthRequest = 150,
+            Margin = new Thickness(0)
+        };
+        image.SetBinding(Image.SourceProperty, "Image");
+        return image;
+    }
+
+    private static Label CreateNameLabel()
+    {
+        var nameLabel = new Label
+        {
+            FontAttributes = FontAttributes.Bold,
+            FontSize = 18,
+            VerticalOptions = LayoutOptions.Start,
+            Margin = new Thickness(10, 10, 10, 0)
+        };
+        nameLabel.SetBinding(Label.TextProperty, "Name");
+        return nameLabel;
+    }
+
+    private static Label CreateAgencyLabel()
+    {
+        var agencyLabel = new Label
+        {
+            FontSize = 16,
+            Margin = new Thickness(10, 0, 10, 10)
+        };
+        agencyLabel.SetBinding(Label.TextProperty, "Agency");
+        return agencyLabel;
+    }
+
     protected override void OnAppearing()
     {
         ViewModel?.LoadCommand.Execute(false).Subscribe();
-        
         base.OnAppearing();
     }
 }
